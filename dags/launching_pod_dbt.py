159055a0a7f955e1601f78306aa22153f8b7e7a9
wesdes <weslley.desouzabarboza@epic.com.mt>
2024-08-28 16:04:06 +0200

    dev

---
+++ dags/launching_pod_dbt.py
@@ -0,0 +1,21 @@
+from pendulum import datetime
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+
+with DAG(
+    dag_id="spining_dbt_pod",
+    schedule="@once",
+    start_date=datetime(2023, 3, 30),
+) as dag:
+    example_kpo = KubernetesPodOperator(
+        kubernetes_conn_id="k8s_conn",
+        image="weslleybarboza/dbt:v1.0",
+        name="spining_dbt_pod",
+        task_id="task-one",
+        is_delete_operator_pod=True,
+        get_logs=True,
+    )
+
+    example_kpo
